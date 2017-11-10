f7e213679aeff024b600e5dcbfbc7e345c428d79
mMmMrnMmMm <32131611+mMmMrnMmMm@users.noreply.github.com>
2017-11-10 16:12:53 -0500

    comments

--- map.py
+++ map.py
@@ -32,11 +32,11 @@ def printmap(map):
     
 class unit():
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.action = 1
-        self.order = []
-        self.actiondelay = 0
+        self.x = x #current location
+        self.y = y 
+        self.action = 1 #what the unit is doing at this moment
+        self.order = [] #what the unit will be doing in the future
+        self.actiondelay = 0 #how long it will be until the current action is complete
         
 printmap(wmap)
 
@@ -44,6 +44,7 @@ def processorder(unit, order, a1, a2):
     if order == 0: #move
         unit.action= 0
         unit.actiondelay = int(.5*wmap[unit.x][unit.y]["terraincost"] + .5*wmap[a1][a2]["terraincost"])
+        #the delay on moving is half of the speed on the current spot and half the speed on the next spot
 
 def processaction(unit):
     if unit.order == []:
@@ -51,12 +52,12 @@ def processaction(unit):
     if not unit.actiondelay == 0:
         unit.actiondelay = unit.actiondelay -1
     else:
-        if unit.action == 0:
+        if unit.action == 0: #moving to 
             unit.x = unit.order[0][1]
             unit.y = unit.order[0][2]
             del unit.order[0]
             processorder(unit, unit.order[0][0], unit.order[0][1], unit.order[0][2])
-        elif unit.action == 1:
+        elif unit.action == 1: #do nothing
             del unit.order[0]
             processorder(unit, unit.order[0][0], unit.order[0][1], unit.order[0][2])
         
